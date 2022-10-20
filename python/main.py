ff1b2383a6227870c7f8e2d4bc075e0b8de245ab
anna <ann.starovoytova@gmail.com>
2022-10-21 10:02:25 +1300

    tuple notes

--- python/main.py
+++ python/main.py
@@ -105,3 +105,56 @@ letters = ['a', 'b', 'c']
 numbers = [1, 2, 3]
 
 letters + numbers
+
+
+from datetime import datetime, timedelta
+ 
+duration = 5
+ 
+run = input("enter s to begin...")
+ 
+period = timedelta(seconds=1)
+ 
+next_time = datetime.now() + period
+ 
+counter = 0
+while run == 's' and counter < duration:
+    if next_time <= datetime.now():
+        print("..", counter)
+        counter += 1
+        # reevaluate the next_time variable
+        next_time += period
+ 
+
+# Tuples
+
+bank_accounts = (("Joe", 33, "234 Great South Road", "022629107"),
+                 ("Tama", 6000),
+                 ("Suzanne", 21025, "45 Green Lane"),
+                 ("Anihera", -75))
+ 
+print("The number of bank accounts is ", len(bank_accounts))
+ 
+# showing names and balances
+for i in bank_accounts:
+    print("\nThe name is ", i[0], " and the balance is $", i[1])
+ 
+# showing only names with addresses
+for i in bank_accounts:
+    if len(i)>2:
+        print("\nThe name is ", i[0], " and the address is ", i[2])
+
+
+# The number of bank accounts is  4
+ 
+# The name is  Joe  and the balance is $ 33
+ 
+# The name is  Tama  and the balance is $ 6000
+ 
+# The name is  Suzanne  and the balance is $ 21025
+ 
+# The name is  Anihera  and the balance is $ -75
+ 
+# The name is  Joe  and the address is  234 Great South Road
+ 
+# The name is  Suzanne  and the address is  45 Green Lane
