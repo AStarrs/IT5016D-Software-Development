fd15847346f8b98f45bdb24225137e809257e96c
anna <ann.starovoytova@gmail.com>
2022-10-25 21:46:04 +1300

    added ticket number counter

--- project/main.py
+++ project/main.py
@@ -1,9 +1,17 @@
 from tickets import Ticket
 
 
-ticket = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket1 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
 
+ticket2 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
 
+ticket3 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket4 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket5 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket6 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket7 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
+ticket8 = Ticket(1, "Anna Starovoytova", "anna@example.com", "My python program is not working")
 
+print(ticket8.print_ticket())
 
-print(ticket.print_ticket())
+# print(ticket1.ticket_number)
--- project/tickets.py
+++ project/tickets.py
@@ -1,22 +1,24 @@
 class Ticket:
 
+
     num_of_tickets = 0
     open_tickets = 0
     resolved_tickets = 0
 
     def __init__(self, staff_id, creator, email, description):
+        Ticket.num_of_tickets += 1
+        Ticket.open_tickets +=1
         self.staff_id = staff_id
         self.creator = creator
         self.email = email
         self.description = description
 
-        Ticket.num_of_tickets += 1
-        Ticket.open_tickets +=1
-
+        self.ticket_number = Ticket.num_of_tickets + 2000
     
     def print_ticket(self):
         '''Displays main information about the ticket'''
         # return '{} {} {} {} {}'.format(self.ID, self.creator, self.email, self.description, self.response)
+        print("Ticket Number:", self.ticket_number)
         print("Staff ID:", self.staff_id)
         print("Creator:", self.creator)
         print("Email:", self.email)
