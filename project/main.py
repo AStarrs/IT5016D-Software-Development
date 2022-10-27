8f8f4f66cb6b503fddf6cb2fcf786b92f13f2415
anna <ann.starovoytova@gmail.com>
2022-10-27 14:23:39 +1300

    created class Main to run the program

--- project/main.py
+++ project/main.py
@@ -1,37 +1,80 @@
 from tickets import Ticket
 
 
-def show_stats():
-    # Print statistic for Submitted, Open, Resoleved tickets
-    print("Number of Tickets Submitted:", Ticket.num_of_tickets)
-    print("Number of Open Tickets:", Ticket.open_tickets)
-    print("Number of Resoleved Tickets:", Ticket.resolved_tickets)
-    print()
+# def show_stats():
+#     # Print statistic for Submitted, Open, Resoleved tickets
+#     print("Number of Tickets Submitted:", Ticket.num_of_tickets)
+#     print("Number of Open Tickets:", Ticket.open_tickets)
+#     print("Number of Resoleved Tickets:", Ticket.resolved_tickets)
+#     print()
     
-def print_all_tickets():
-    # Print all tickets information
-    for ticket in Ticket.tickets:
-        ticket.print_ticket()
+# def print_all_tickets():
+#     # Print all tickets information
+#     for ticket in Ticket.tickets:
+#         ticket.print_ticket()
 
-ticket1 = Ticket("ANNAS", "Anna Starovoytova", "annas@example.com", "Printer is not working")
+# ticket1 = Ticket("ANNAC", "Anna Campbel", "annac@example.com", "Printer is not working")
 
-ticket2 = Ticket("OLGAN", "Olga Newman", "olgan@example.com", "Need Password Change")
+# ticket2 = Ticket("OLGAN", "Olga Newman", "olgan@example.com", "Need Password Change")
 
-ticket3 = Ticket("JOHNW", "John Wright", "johnw@example.com", "Request for new monitor")
-
-
-show_stats()
-print_all_tickets()
+# ticket3 = Ticket("JOHNW", "John Wright", "johnw@example.com", "Request for new monitor")
 
 
-ticket1.respond_to_ticket("Printer connection fixed")
-show_stats()
-print_all_tickets()
+# show_stats()
+# print_all_tickets()
 
-ticket1.reopen_ticket("Software update required")
-show_stats()
-print_all_tickets()
 
+# ticket1.respond_to_ticket("Printer connection fixed")
+# show_stats()
+# print_all_tickets()
+
+# ticket1.reopen_ticket("Software update required")
+# show_stats()
+# print_all_tickets()
+
+
+
+class Main:
+    def __init__(self):
+        self.name = "Ticketing System"
+    
+    def show_stats(self):
+        # Print statistic for Submitted, Open, Resoleved tickets
+        print("Number of Tickets Submitted:", Ticket.num_of_tickets)
+        print("Number of Open Tickets:", Ticket.open_tickets)
+        print("Number of Resoleved Tickets:", Ticket.resolved_tickets)
+        print()
+    
+    def print_all_tickets(self):
+        # Print all tickets information
+        for ticket in Ticket.tickets:
+            ticket.print_ticket(self)
+
+    def run(self):
+        print("Create Ticket")
+
+        self.ticket1 = Ticket("ANNAC", "Anna Campbel", "annac@example.com", "Printer is not working")
+
+        self.ticket2 = Ticket("OLGAN", "Olga Newman", "olgan@example.com", "Need Password Change")
+
+        self.ticket3 = Ticket("JOHNW", "John Wright", "johnw@example.com", "Request for new monitor")
+
+        self.show_stats()
+        self.print_all_tickets()
+
+        print("Respond to Ticket 2001")
+        # Respond and resolve issue for ticket1, close and change stats
+        self.ticket1.respond_to_ticket("Printer connection fixed")
+        
+        self.show_stats()
+        self.print_all_tickets()
+
+        print("Reopen ticket 2002")
+        # Reopen closed ticket2, change stats
+        self.ticket2.reopen_ticket("Software update required")
+
+        self.show_stats()
+        self.print_all_tickets()
 
 
 
