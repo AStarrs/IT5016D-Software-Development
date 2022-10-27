deef187f1a1f6366c51f7a31548cffbe5c025835
anna <ann.starovoytova@gmail.com>
2022-10-28 10:01:09 +1300

    Changed show_stats method name to ticket_stats, added comments to the code

--- project/main.py
+++ project/main.py
@@ -1,44 +1,10 @@
 from tickets import Ticket
 
-
-# def show_stats():
-#     # Print statistic for Submitted, Open, Resoleved tickets
-#     print("Number of Tickets Submitted:", Ticket.num_of_tickets)
-#     print("Number of Open Tickets:", Ticket.open_tickets)
-#     print("Number of Resoleved Tickets:", Ticket.resolved_tickets)
-#     print()
-    
-# def print_all_tickets():
-#     # Print all tickets information
-#     for ticket in Ticket.tickets:
-#         ticket.print_ticket()
-
-# ticket1 = Ticket("ANNAC", "Anna Campbel", "annac@example.com", "Printer is not working")
-
-# ticket2 = Ticket("OLGAN", "Olga Newman", "olgan@example.com", "Need Password Change")
-
-# ticket3 = Ticket("JOHNW", "John Wright", "johnw@example.com", "Request for new monitor")
-
-
-# show_stats()
-# print_all_tickets()
-
-
-# ticket1.respond_to_ticket("Printer connection fixed")
-# show_stats()
-# print_all_tickets()
-
-# ticket1.reopen_ticket("Software update required")
-# show_stats()
-# print_all_tickets()
-
-
-
 class Main:
     def __init__(self):
         self.name = "Ticketing System"
     
-    def show_stats(self):
+    def ticket_stats(self):
         # Print statistic for Submitted, Open, Resoleved tickets
         print("Number of Tickets Submitted:", Ticket.num_of_tickets)
         print("Number of Open Tickets:", Ticket.open_tickets)
@@ -51,29 +17,31 @@ class Main:
             ticket.print_ticket()
 
     def run(self):
+        # Run the program
         print("========== Creating Tickets ==========\n")
-
+        # Creating 3 tickets, ticket2 autoresolve the password change and change
+        # status to closed
         self.ticket1 = Ticket("ANNAC", "Anna Campbel", "annac@example.com", "Printer is not working")
 
         self.ticket2 = Ticket("OLGAN", "Olga Newman", "olgan@example.com", "Need Password Change")
 
         self.ticket3 = Ticket("JOHNW", "John Wright", "johnw@example.com", "Request for new monitor")
 
-        self.show_stats()
+        self.ticket_stats()
         self.print_all_tickets()
 
         print("========== Respond to Ticket 2001 ==========\n")
         # Respond and resolve issue for ticket1, close and change stats
         self.ticket1.respond_to_ticket("Printer connection fixed")
         
-        self.show_stats()
+        self.ticket_stats()
         self.print_all_tickets()
 
-        print("========== Reopen ticket 2002 ==========")
-        # Reopen closed ticket2, change stats
+        print("========== Reopen ticket 2002 ==========\n")
+        # Reopen closed ticket2, add response, change stats and status
         self.ticket2.reopen_ticket("Software update required")
 
-        self.show_stats()
+        self.ticket_stats()
         self.print_all_tickets()
 
 
